38ae57cdade22306dcd48993f96853477378574a
MikaTarro <becoool2013@mail.ru>
2024-11-13 08:55:44 +0100

    inj:  o Create pjct  o Create base tmp's  o Setup SexWInd  o Create NINJAmacros*

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home/index.html")
+
+
+
+@app.route("/posts/")
+def posts():
+    return render_template("posts/index.html")
