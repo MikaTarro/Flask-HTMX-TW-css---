8d7daf9b0ac954ac603e8114da4a141970c11603
MikaTarro <becoool2013@mail.ru>
2024-11-14 07:19:37 +0100

    done!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Flask-HTMX-TW-css---
-Простая лента постов * 
+тест лента * 
+## 🛠🛠🛠🛠🛠
+# tailwind
++ https://flowbite.com/docs/getting-started/quickstart/
+# htmx
++ https://htmx.org/docs/#installing
+## Запуск проекта
++ venv -->
++ flask --app main run --debug
++ npx tailwindcss -i ./static/src/input.css -o ./static/dist/css/output.css --watch
+![img.png](img.png)
+## 🛠🛠🛠🛠🛠🛠
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from . import db
 
 app = Flask(__name__)
 
@@ -8,7 +10,30 @@ def home():
     return render_template("home/index.html")
 
 
-
 @app.route("/posts/")
 def posts():
-    return render_template("posts/index.html")
+    posts = db.get_posts()
+    return render_template("posts/index.html", posts=posts)
+
+
+@app.route("/post/<string:post_id>")
+def post_view(post_id):
+    post = db.get_post(post_id)
+    return render_template("posts/_partials/_post.html", post=post)
+
+@app.route("/post/delete/<string:post_id>", methods=["DELETE"])
+def post_delete(post_id):
+    if request.method == "DELETE":
+        db.delete_post(post_id)
+        return ""
+
+
+@app.route("/posts/edit/<string:post_id>", methods=["GET", "PUT"])
+def post_edit(post_id):
+    if request.method == "PUT":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        db.update_post(post_id, title, content)
+        return post_view(post_id)
+    post = db.get_post(post_id)
+    return render_template("posts/_partials/_edit.html", post=post)
